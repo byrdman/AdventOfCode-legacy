96f979bd847b2f5538e02eb5f031d8232651e40a
Sam Byrd <sbyrd@euronetworldwide.com>
2019-12-13 16:02:00 -0600

    day 13 work

---
+++ AOC-2019/src/test/java/net/thebyrdnest/aoc/aoc2019/tests/Day13Test.java
@@ -0,0 +1,36 @@
+package net.thebyrdnest.aoc.aoc2019.tests;
+
+import net.thebyrdnest.aoc.aoc2019.Day13;
+import org.junit.jupiter.api.*;
+
+public class Day13Test {
+
+
+    @BeforeAll
+    public static void setupBeforeClass() throws Exception {
+
+    }
+
+    @AfterAll
+    public static void teardownAfterClass() throws Exception {
+
+    }
+
+    @BeforeEach
+    public void setupBeforeEach() throws Exception {
+
+    }
+
+    @AfterEach
+    public void tearDownAfterEach() throws Exception {
+
+    }
+
+    @Test
+    public void Problem1() {
+
+        Day13 solver = new Day13();
+        int paddles = solver.solve1();
+        Assertions.assertEquals(1747, paddles);
+    }
+}
